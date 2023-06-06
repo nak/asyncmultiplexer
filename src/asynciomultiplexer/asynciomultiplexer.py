2cdb7629bc98364559357de8807f23f373ad2b04
nak <jrusnak69@gmail.com>
2023-06-05 21:04:33 -0700

    fix testing; add ability to handle orphaned items

--- src/asynciomultiplexer/asynciomultiplexer.py
+++ src/asynciomultiplexer/asynciomultiplexer.py
@@ -4,7 +4,8 @@ multiplexing utils for parallel (async) tasks
 
 import asyncio
 import queue
-from typing import AsyncIterator, TypeVar, Generic
+from contextlib import suppress
+from typing import AsyncIterator, TypeVar, Generic, Optional, Callable, AsyncGenerator
 
 __all__ = ["AsyncMultiplexedIterator", "AsyncAdaptorQueue"]
 T = TypeVar('T')
@@ -15,8 +16,9 @@ class AsyncMultiplexedIterator(Generic[T]):
     Class for multiplexing multiple async iterators in parallel into a single async iterator
 
     >>> iterators: AsyncIterator[T] = ...
-    ... async for device_data in AsyncMultiplexedIterator(*iterators):
-    ...     yield device_data
+    ... with  AsyncMultiplexedIterator(*iterators) as multiplexre:
+    ...     async for device_data in AsyncMultiplexedIterator(*iterators):
+    ...         yield device_data
     """
 
     class Sentinel:
@@ -30,48 +32,72 @@ class AsyncMultiplexedIterator(Generic[T]):
         def iterator(self) -> AsyncIterator[T]:
             return self._iterator
 
-    def __init__(self, *iterators: AsyncIterator[T], timeout=0):
+    def __init__(self, *iterators: AsyncIterator[T], timeout=0,
+                 handle_orphan: Optional[Callable[[T], None]] = None):
         """
         :param iterators: which iterators to iterate over in parallel
         :param timeout: timeout in seconds to wait on next item, or default/zero to wait indefinitely
+        :param handle_orphan: optional callback to handle orphaned items.  An itme is orphaned when
+            the multiplexer exits early but the client inserts an item into the multiplexing queue
+            AFTER the multiplexer stops processing the queue but BEFORE it has properly shutdown
+            all the workers.  Any exceptions from this handler will be ignored and not propagated.
         """
         self._iterators = list(iterators)
         self._multiplexing_q: asyncio.Queue[T] = asyncio.Queue()
         self._timeout = timeout
+        self._handle_orphan = handle_orphan
+        self._active = False
+
+    async def __aenter__(self):
+        return self
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        self._active = False
+        for task in [t for t in self._tasks if not t.done()]:
+            await task
+        while self._handle_orphan is not None:  # always True or False
+            try:
+                item = self._multiplexing_q.get_nowait()
+                with suppress(Exception):
+                    self._handle_orphan(item)
+            except asyncio.queues.QueueEmpty:
+                break
 
     def __aiter__(self) -> "AsyncMultiplexedIterator[T]":
         async def worker(iterator: AsyncIterator[T]):
             try:
                 async for item in iterator:
+                    if not self._active:
+                        break
                     await self._multiplexing_q.put(item)
-            except BaseException as e:
+            except Exception as e:
                 await self._multiplexing_q.put(e)
+                raise
             finally:
-                await self._multiplexing_q.put(self.Sentinel(iterator))
+                if self._active:
+                    await self._multiplexing_q.put(self.Sentinel(iterator))
+
         self._tasks = [
             asyncio.create_task(worker(iterator)) for iterator in self._iterators
         ]
+        self._active = True
         return self
 
     async def __anext__(self) -> T:
-        try:
-            while self._iterators:
-                if self._timeout > 0:
-                    next_item = await asyncio.wait_for(self._multiplexing_q.get(), timeout=self._timeout)
-                else:
-                    next_item = await self._multiplexing_q.get()
-                if isinstance(next_item, BaseException):
-                    raise StopAsyncIteration() from next_item
-                elif isinstance(next_item, self.Sentinel):
-                    self._iterators.remove(next_item.iterator)
-                    if not self._iterators:
-                        raise StopAsyncIteration()
-                    continue
-                return next_item
-        except Exception:
-            for task in [t for t in self._tasks if not t.done()]:
-                task.cancel()
-            raise
+        while self._iterators:
+            if self._timeout > 0:
+                next_item = await asyncio.wait_for(self._multiplexing_q.get(), timeout=self._timeout)
+            else:
+                next_item = await self._multiplexing_q.get()
+            if isinstance(next_item, BaseException):
+                raise StopAsyncIteration() from next_item
+            elif isinstance(next_item, self.Sentinel):
+                self._iterators.remove(next_item.iterator)
+                if not self._iterators:
+                    self._active = False
+                    raise StopAsyncIteration()
+                continue
+            return next_item
 
 
 class AsyncAdaptorQueue(Generic[T]):
